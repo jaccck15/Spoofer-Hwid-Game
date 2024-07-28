4e9c4ee839aaa59a46af06539822ec24fcf07a5d
siauhads <hsdfh87yaudh@outlook.com>
2024-07-28 11:58:47 +0400

    Upload Files

---
+++ Simple Hwid Spoofer/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Simple Hwid Spoofer")]
+[assembly: AssemblyDescription("Hwid Spoofer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Simple Hwid Spoofer")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("72e19eba-79b9-4195-9077-ed50e9a8f182")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
